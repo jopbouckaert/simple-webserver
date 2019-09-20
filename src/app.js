21bf4f581e84acb68615603e429952c50b720433
Jop Bouckaert <jop.bouckaert@outlook.com>
2019-09-20 14:46:12 +0200

    use express

--- src/app.js
+++ src/app.js
@@ -1,22 +1,24 @@
 const http = require("http");
-const server = http.createServer();
 const url = require("url");
 
-function set_response(response, content, status = 200) {
-  response.writeHead(status, { "Content-Type": "text/plain" });
+const server = http.createServer();
+
+const set_response = (response, content) => {
+  response.writeHead(200, { "Content-Type": "text/plain" });
   response.write(content);
-}
+};
 
 server.on("request", (request, response) => {
   let path = url.parse(request.url).pathname;
   console.log(path);
 
   if (path === "/") {
-    set_response(response, "Hello World!");
+    set_response(response, "Hello world");
   } else if (path === "/about") {
-    set_response(response, "Made by VIVES Students!");
+    set_response(response, "Created by VIVES peoples");
   } else {
-    set_response(response, "Error", 418);
+    response.writeHead(404, { "Content-Type": "text/plain" });
+    response.write("Error page");
   }
   response.end();
 });
