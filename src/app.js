71770813ce32e2aeb5086ac4503f1c83c2cf884a
Jop Bouckaert <jop.bouckaert@outlook.com>
2019-09-20 14:38:55 +0200

    add routing

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 const http = require("http");
 const server = http.createServer();
+const url = require("url");
+
+function set_response(response, content, status = 200) {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.write(content);
+}
 
 server.on("request", (request, response) => {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.write("Hello world");
+  let path = url.parse(request.url).pathname;
+  console.log(path);
+
+  if (path === "/") {
+    set_response(response, "Hello World!");
+  } else if (path === "/about") {
+    set_response(response, "Made by VIVES Students!");
+  } else {
+    set_response(response, "Error", 418);
+  }
   response.end();
 });
 
